dabdacc3ca8de1fe1bebeae7859012d0726db420
HelaLetsGo <jonascelkiss@gmail.com>
2025-06-26 17:14:53 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [], // Jei naudojami išoriniai paveikslėliai, įrašyk čia domenus pvz.: ["example.com"]
+  },
 };
 
 module.exports = nextConfig;
